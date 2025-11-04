5cfe607761f221de3167b9dff8efc1f950467ff0
Jayesh Manani <jmanani@3-E-20.42heilbronn.de>
2025-11-04 15:19:06 +0100

    added: baseline

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 15:15:45 by jmanani           #+#    #+#             */
+/*   Updated: 2025/11/04 15:17:36 by jmanani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
