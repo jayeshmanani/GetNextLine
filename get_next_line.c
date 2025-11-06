faf9132b2be23091f40a8e4e87fc66066529da8a
Jayesh Manani <jmanani@3-C-7.42heilbronn.de>
2025-11-06 20:09:25 +0100

    upda: workinhg without mem leak

--- get_next_line.c
+++ get_next_line.c
@@ -6,96 +6,11 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 15:15:25 by jmanani           #+#    #+#             */
-/*   Updated: 2025/11/06 20:08:17 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/11/06 20:09:19 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	count;
-
-	count = 0;
-	while (s[count] != '\0')
-		count++;
-	return (count);
-}
-
-char	*ft_strdup(const char *src)
-{
-	int		len;
-	char	*dup;
-	int		i;
-
-	len = ft_strlen(src);
-	dup = (char *)malloc(len + 1);
-	if (!dup)
-		return (0);
-	i = -1;
-	while (++i < len)
-		dup[i] = src[i];
-	dup[i] = '\0';
-	return (dup);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s != '\0')
-	{
-		if (*s++ == (char)c)
-			return ((char *)(s - 1));
-	}
-	if (*s == '\0' && (char)c != '\0')
-		return (NULL);
-	return ((char *)s);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	int		len;
-	char	*new_str;
-
-	if (!s1 || !s2)
-		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2);
-	if (!len)
-		return (ft_strdup(""));
-	new_str = (char *)malloc(sizeof(char) * (len + 1));
-	if (!new_str)
-		return (NULL);
-	while (*s1)
-		*(new_str++) = *(s1++);
-	while (*s2)
-		*(new_str++) = *(s2++);
-	*new_str = '\0';
-	return (new_str - len);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*new_str;
-	size_t	str_len;
-	size_t	actual_len;
-	size_t	i;
-
-	actual_len = len;
-	i = 0;
-	str_len = ft_strlen(s);
-	actual_len = str_len - start;
-	if (!*s || str_len < actual_len || start > str_len || len == 0
-		|| actual_len == 0)
-		return (ft_strdup(""));
-	if (actual_len > len)
-		actual_len = len;
-	new_str = (char *)malloc(sizeof(char) * (actual_len + 1));
-	if (!new_str)
-		return (0);
-	while (*(s + start) && i < actual_len)
-		*new_str++ = *(s + start + (i++));
-	*new_str = '\0';
-	return (new_str - actual_len);
-}
 
 char	*get_next_line(int fd)
 {
