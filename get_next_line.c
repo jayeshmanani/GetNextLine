72e36c9a94c98ab542f21740b80a95ab2baf8583
Jayesh Manani <jmanani@3-C-7.42heilbronn.de>
2025-11-07 19:44:38 +0100

    working code

--- get_next_line.c
+++ get_next_line.c
@@ -6,110 +6,11 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 15:15:25 by jmanani           #+#    #+#             */
-/*   Updated: 2025/11/07 19:41:49 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/11/07 19:44:01 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-// char	*ft_substr(char *s, unsigned int start, size_t len)
-// {
-// 	char	*res;
-// 	size_t	i;
-// 	size_t	s_len;
-
-// 	if (!s)
-// 		return (NULL);
-// 	s_len = ft_strlen(s);
-// 	if (start >= s_len)
-// 		return (free(s), ft_strdup(""));
-// 	if (len > s_len - start)
-// 		len = s_len - start;
-// 	res = (char *)malloc(len + 1);
-// 	if (!res)
-// 		return (free(s), NULL);
-// 	i = 0;
-// 	while (i < len && s[start + i])
-// 	{
-// 		res[i] = s[start + i];
-// 		i++;
-// 	}
-// 	res[i] = '\0';
-// 	free(s);
-// 	return (res);
-// }
-
-// size_t	ft_strlen(const char *s)
-// {
-// 	size_t	count;
-
-// 	count = 0;
-// 	if (!s)
-// 		return (count);
-// 	while (s[count] != '\0')
-// 		count++;
-// 	return (count);
-// }
-
-// char	*ft_strjoin(char *s1, char const *s2)
-// {
-// 	int		len;
-// 	char	*new_str;
-// 	char	*temp;
-
-// 	temp = s1;
-// 	if (!s1 && !s2)
-// 		return (NULL);
-// 	if (!s1)
-// 		return (ft_strdup(s2));
-// 	if (!s2)
-// 		return (s1);
-// 	len = ft_strlen(s1) + ft_strlen(s2);
-// 	new_str = (char *)malloc(sizeof(char) * (len + 1));
-// 	if (!new_str)
-// 		return (free(temp), NULL);
-// 	while (*s1 != '\0')
-// 		*(new_str++) = *(s1++);
-// 	while (*s2 != '\0')
-// 		*(new_str++) = *(s2++);
-// 	*new_str = '\0';
-// 	free(temp);
-// 	return (new_str - len);
-// }
-
-// char	*ft_strdup(const char *src)
-// {
-// 	int		len;
-// 	char	*dup;
-// 	int		i;
-
-// 	if (!src)
-// 		return (NULL);
-// 	len = ft_strlen(src);
-// 	dup = (char *)malloc(len + 1);
-// 	if (!dup)
-// 		return (NULL);
-// 	i = -1;
-// 	while (++i < len)
-// 		dup[i] = src[i];
-// 	dup[i] = '\0';
-// 	return (dup);
-// }
-
-// char	*ft_strchr(const char *s, int c)
-// {
-// 	if (!s)
-// 		return (NULL);
-// 	while (*s)
-// 	{
-// 		if (*s == (char)c)
-// 			return ((char *)(s));
-// 		s++;
-// 	}
-// 	if (*s == '\0' && (char)c != '\0')
-// 		return (NULL);
-// 	return ((char *)s);
-// }
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
